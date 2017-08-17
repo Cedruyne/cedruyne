4699a2c33f77cee6ccce681ee7ed8c6d2d1dafdd
Cédric de Bruyne <cdebruyne@outlook.com>
2017-08-17 10:00:26 +0200

    modification du comportement du header content - hidden lorsque le scroll position est supérieur à 170

--- js/header.js
+++ js/header.js
@@ -13,4 +13,12 @@ function headerVisibility() {
         'margin-top': (scrollPos / 4) + "px",
         'opacity': 1 - (scrollPos / 200)
     })
+
+    if (scrollPos > 170)
+    {
+        $('.headerContent').addClass('hidden');
+    } else
+    {
+        $('.headerContent').removeClass('hidden');
+    }
 }
