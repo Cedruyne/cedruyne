6360bd21afb6984af0dc23b5f78f6b2bb25dad64
Cédric de Bruyne <cdebruyne@outlook.com>
2017-08-17 11:40:43 +0200

    Ajout de Bootstrap + tether pour gérer la Legal Modal + correction du headerVisibility sur un rechargement de page

--- js/header.js
+++ js/header.js
@@ -9,13 +9,15 @@ function heightBackground(){
 
 function headerVisibility() {
     var scrollPos = $(window).scrollTop();
+    var currentPos = window.pageYOffset;
     $('.headerContent').css({
         'margin-top': (scrollPos / 4) + "px",
         'opacity': 1 - (scrollPos / 200)
     })
 
-    if (scrollPos > 170)
+    if ((scrollPos > 110) || (currentPos > 110))
     {
+        console.log("Scroll Pos : " + scrollPos + "Current Pos : " + currentPos);
         $('.headerContent').addClass('hidden');
     } else
     {
